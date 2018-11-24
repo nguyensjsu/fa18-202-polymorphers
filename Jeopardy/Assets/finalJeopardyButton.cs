a31ed20c476302b28da40eb4fa4bff165095ff37
CSIYI <siyi.cai@sjsu.edu>
2018-11-24 14:32:35 -0800

    add UI

---
+++ Jeopardy/Assets/finalJeopardyButton.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class finalJeopardyButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Button b = GetComponent<Button>();
+        AudioSource finalJeopardyAudio = GetComponent<AudioSource>();
+        b.onClick.AddListener(delegate () { finalJeopardyAudio.Play(); });
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
