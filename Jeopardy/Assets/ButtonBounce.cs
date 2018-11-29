1fe110f840970458d9ea5b9ccec11e1d291bcd65
David Montes <david.montes@sjsu.edu>
2018-11-29 10:20:47 -0800

    Added button hover animation

---
+++ Jeopardy/Assets/ButtonBounce.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonBounce : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        animator.SetBool("isHover", true);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        animator.SetBool("isHover", false);
+    }
+}
