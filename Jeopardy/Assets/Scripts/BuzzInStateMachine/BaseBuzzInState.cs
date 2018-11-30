b906f9fb07337f5b6c0ef92dbf78a97e7b0d7051
David Montes <david.montes@sjsu.edu>
2018-11-30 10:21:08 -0800

    added detection for controller input

---
+++ Jeopardy/Assets/Scripts/BuzzInStateMachine/BaseBuzzInState.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseBuzzInState : StateMachineBehaviour
+{
+    protected AIBuzzIn ai;
+
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        ai = animator.gameObject.GetComponent<AIBuzzIn>();
+    }
+}
